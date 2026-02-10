ac133b6ebaeed1c25ee58efcd0b40ffb9665fd72
ojhaji-clg <so3864@srmist.edu.in>
2026-02-10 09:58:38 +0530

    Hash Tables Practice

---
+++ Hash Tables/LibrarySystem.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class LibrarySystem {
+    public static void main(String[] args) {
+        // ISBN (key) -> Book title (value)
+        HashMap<String, String> library = new HashMap<>();
+
+        library.put("1234", "Java");
+        library.put("5678", "CPP");
+        library.put("9012", "Python");
+
+        //O(1) lookup
+
+        System.out.println(library.get("1234"));
+
+        //Load factor = no of entries / number of buckets
+        // default is  0.75
+        System.out.println("Size: " + library.size());
+    }
+}
